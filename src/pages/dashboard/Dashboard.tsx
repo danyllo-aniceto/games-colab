af193e1d9713cd3ce94b4242be917cc53e407957
danyllo-aniceto <danyllo.aniceto.carvalho@gmail.com>
2022-07-25 16:00:27 -0300

    style of dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,81 @@
 import { BaseLayout } from '../../layout/BaseLayout'
+import {
+  Box,
+  Card,
+  Container,
+  ContentTopGames,
+  Header,
+  Content,
+  Text
+} from './styles'
 
 export function Dashboard() {
   return (
     <BaseLayout>
-      <h1>404 Not Found</h1>
+      <Container>
+        <Header>
+          <h1>Bem Vindo!</h1>
+          <h3>
+            Sistema colaborativo para cadastramento, avaliação e consulta de
+            jogos
+          </h3>
+        </Header>
+        <ContentTopGames>
+          <Text>
+            <p>
+              &nbsp;O <strong>Meu Game Favorito</strong> tem como objetivo
+              exibir uma avaliação geral do público de games sobre os jogos
+              lançados para consoles como: Playstation, Xbox, NintendoSwith,
+              Windows, etc.
+            </p>
+            <p>
+              &nbsp;Cadastre jogos colocando as informações necessárias, avalie
+              os jogos deixando sua nota e um comentário e veja quais jogos
+              fazem mais sucesso entre o público!!!
+            </p>
+            <h1>TOP 3 JOGOS 🎮</h1>
+          </Text>
+          <Card>
+            <Box>
+              <Content>
+                <h2>1º</h2>
+                <h3>God Of War</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Labore, totam velit? Iure nemo labore inventore
+                </p>
+                <a href="#">Veja mais</a>
+              </Content>
+            </Box>
+          </Card>
+          <Card>
+            <Box>
+              <Content>
+                <h2>2º</h2>
+                <h3>God Of War</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Labore, totam velit? Iure nemo labore inventore
+                </p>
+                <a href="#">Veja mais</a>
+              </Content>
+            </Box>
+          </Card>
+          <Card>
+            <Box>
+              <Content>
+                <h2>3º</h2>
+                <h3>God Of War</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Labore, totam velit? Iure nemo labore inventore
+                </p>
+                <a href="#">Veja mais</a>
+              </Content>
+            </Box>
+          </Card>
+        </ContentTopGames>
+      </Container>
     </BaseLayout>
   )
 }
--- src/pages/dashboard/styles.ts
+++ src/pages/dashboard/styles.ts
@@ -1,1 +1,145 @@
 import styled from 'styled-components'
+
+import godOfWarImg from '../../assets/godOfWar.jpg'
+
+export const Container = styled.div`
+  background: #dee9ffd1;
+  height: auto;
+  margin: 5px 2px;
+  padding: 5px 8px;
+`
+export const Header = styled.div`
+  font-family: 'Mulish', sans-serif;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+
+  h1 {
+    font-size: 3rem;
+  }
+`
+export const ContentTopGames = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  max-width: 1200px;
+  margin: 40px 0;
+`
+
+export const Text = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  font-family: Arial, Helvetica, sans-serif;
+
+  h1 {
+    font-size: 50px;
+    margin-top: 25px;
+    text-align: center;
+    font-family: 'Righteous', cursive;
+    color: transparent;
+    -webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: var(--header-color-blue);
+  }
+`
+
+export const Card = styled.div`
+  position: relative;
+  min-width: 320px;
+  height: 440px;
+  box-shadow: inset 5px 5px 5px rgba(0, 0, 0, 0.2),
+    inset -5px -5px 15px rgba(255, 255, 255, 0.1),
+    5px 5px 15px rgba(0, 0, 0, 0.3), -5px -5px 15px rgba(255, 255, 255, 0.1);
+  border-radius: 15px;
+  margin: 30px;
+  margin-bottom: 15px;
+  transition: 0.5s;
+
+  :nth-child(1) a {
+    background: #2196f3;
+  }
+  :nth-child(2) a {
+    background: #e91e63;
+  }
+  :nth-child(3) a {
+    background: #23c186;
+  }
+`
+export const Box = styled.div`
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  right: 20px;
+  bottom: 20px;
+  background-image: url(${godOfWarImg});
+  background-size: cover;
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  transition: 0.5s;
+
+  :hover {
+    transform: translateY(-50px);
+  }
+
+  ::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 50%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.03);
+  }
+`
+
+export const Content = styled.div`
+  padding: 20px;
+  text-align: center;
+
+  h2 {
+    position: absolute;
+    top: -10px;
+    right: 30px;
+    font-size: 8rem;
+    color: rgb(255 255 255 / 43%);
+  }
+
+  h3 {
+    font-size: 1.8rem;
+    color: #fff;
+    z-index: 1;
+    transition: 0.5s;
+    margin-bottom: 15px;
+  }
+
+  p {
+    font-size: 1rem;
+    font-weight: 300;
+    color: rgba(255, 255, 255, 0.9);
+    z-index: 1;
+    transition: 0.5s;
+  }
+
+  a {
+    position: relative;
+    display: inline-block;
+    padding: 8px 20px;
+    background: black;
+    border-radius: 5px;
+    text-decoration: none;
+    color: white;
+    margin-top: 20px;
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+    transition: 0.5s;
+
+    :hover {
+      box-shadow: 0 10px 20px rgba(0, 0, 0, 0.6);
+      background: #fff;
+      color: #000;
+    }
+  }
+`
