e99fba6b6bb892f3f51fa08b03f4633ca924bfde
danyllo-aniceto <danyllo.aniceto.carvalho@gmail.com>
2022-07-28 16:49:08 -0300

    28-07-2022

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,21 +5,17 @@ import { Games } from '../pages/games/Games'
 import { Dashboard } from '../pages/dashboard/Dashboard'
 import { Users } from '../pages/users/Users'
 
-
-
 export function Router() {
   return (
-    
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/consoles" element={<Consoles />} />
-          <Route path="/games" element={<Games />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="*" element={<Navigate to="/dashboard" />} />
-        </Routes>
-      </BrowserRouter>
-  
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/consoles" element={<Consoles />} />
+        <Route path="/games" element={<Games />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="*" element={<Navigate to="/dashboard" />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
