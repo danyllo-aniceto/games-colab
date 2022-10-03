560680c81e32a615c31b2b2dbeffb80e3a0c93b1
danyllo-aniceto <danyllo.aniceto.carvalho@gmail.com>
2022-10-03 17:37:43 -0300

    create dialog component

--- src/components/StyledDialog/styles.ts
+++ src/components/StyledDialog/styles.ts
@@ -1,17 +1,5 @@
 import styled from 'styled-components'
 import { Dialog, DialogTitle } from '@mui/material'
-import { InputField } from '../../../components/InputField'
-
-export const ContentRadio = styled.div`
-  margin: 30px 0 20px 0;
-  display: flex;
-  flex-direction: row;
-  gap: 50px;
-
-  input {
-    margin-right: 5px;
-  }
-`
 
 export const StyledDialog = styled(Dialog)`
   .css-1t1j96h-MuiPaper-root-MuiDialog-paper {
@@ -27,7 +15,7 @@ export const Title = styled(DialogTitle)`
   h1 {
     font-family: 'Righteous', cursive;
     font-size: 50px;
-    /* color: var(--header-color-blue); */
+    color: var(--header-color-blue);
   }
 `
 
@@ -35,9 +23,4 @@ export const Image = styled.img`
   max-width: 120px;
   border-radius: 50px;
   box-shadow: 0px 2px 15px 3px rgba(0, 0, 0, 0.5);
-`
-export const Label = styled.label`
-  .css-1x51dt5-MuiInputBase-input-MuiInput-input {
-    color: red;
-  }
 `
--- src/pages/games/DialogCreateGame/index.tsx
+++ src/pages/games/DialogCreateGame/index.tsx
@@ -4,6 +4,7 @@ import { InputField } from '../../../components/InputField'
 import { IGameDTO } from '../../../dtos/IGameDTO'
 import { ContentRadio, StyledDialog, Image, Title, Label } from './styles'
 import sonicImg from './../../../assets/sonic-videogame.png'
+import { Dialog } from '../../../components/StyledDialog'
 
 // teste
 
@@ -25,90 +26,87 @@ export function DialogCreateGame({
   const [showInputUpload, setShowInputUpload] = useState(false)
 
   return (
-    <StyledDialog open={open} onClose={onClose}>
-      <Title>
-        <h1>Cadastrar Jogo</h1>
-
-        <Image src={sonicImg} />
-      </Title>
-      <DialogContent>
-        <InputField
-          value={game.name}
-          onChange={onChange}
-          label="Nome"
-          name={'name'}
-        />
-        <InputField
-          value={game.developer}
-          onChange={onChange}
-          label="Desenvolvedor"
-          name={'developer'}
-        />
-        <InputField
-          value={game.summary}
-          onChange={onChange}
-          label="Descrição"
-          name={'summary'}
-        />
-        <InputField
-          value={game.genre}
-          onChange={onChange}
-          label="Gênero"
-          name={'genre'}
-        />
-        {/** Problema de tipagem: Input não pode receber array trocar por um selector */}
-        {/* <InputField
+    <Dialog onClose={onClose} open={open} title="Cadastrar Jogo">
+      <>
+        <DialogContent>
+          <InputField
+            value={game.name}
+            onChange={onChange}
+            label="Nome"
+            name={'name'}
+          />
+          <InputField
+            value={game.developer}
+            onChange={onChange}
+            label="Desenvolvedor"
+            name={'developer'}
+          />
+          <InputField
+            value={game.summary}
+            onChange={onChange}
+            label="Descrição"
+            name={'summary'}
+          />
+          <InputField
+            value={game.genre}
+            onChange={onChange}
+            label="Gênero"
+            name={'genre'}
+          />
+          {/** Problema de tipagem: Input não pode receber array trocar por um selector */}
+          {/* <InputField
           label="Plataforma"
           name="idPlatform"
           onChange={onChange}
           value={game.idPlatform}
         /> */}
 
-        <ContentRadio>
-          <Label>
-            <input
-              type="radio"
-              name="radio_image"
-              value="upload"
-              onChange={onChange}
-              onClick={() => setShowInputUpload(true)}
-            />
-            Upload
-          </Label>
+          <ContentRadio>
+            <Label>
+              <input
+                type="radio"
+                name="radio_image"
+                value="upload"
+                onChange={onChange}
+                onClick={() => setShowInputUpload(true)}
+              />
+              Upload
+            </Label>
 
-          <label>
-            <input
-              type="radio"
-              name="radio_image"
-              value="link"
-              onChange={onChange}
-              onClick={() => setShowInputUpload(false)}
-            />
-            Link
-          </label>
-        </ContentRadio>
+            <label>
+              <input
+                type="radio"
+                name="radio_image"
+                value="link"
+                onChange={onChange}
+                onClick={() => setShowInputUpload(false)}
+              />
+              Link
+            </label>
+          </ContentRadio>
 
-        {showInputUpload ? (
-          <InputField
-            value={game.file}
-            type="file"
-            onChange={onChange}
-            label="Link da imagem"
-            name="file"
-          />
-        ) : (
-          <InputField
-            value={game.image}
-            onChange={onChange}
-            label="Link da imagem"
-            name="image"
-          />
-        )}
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose}>Cancelar</Button>
-        <Button onClick={onSubmitCreate}>Cadastrar</Button>
-      </DialogActions>
-    </StyledDialog>
+          {showInputUpload ? (
+            <InputField
+              value={game.file}
+              type="file"
+              onChange={onChange}
+              label="Link da imagem"
+              name="file"
+            />
+          ) : (
+            <InputField
+              value={game.image}
+              onChange={onChange}
+              label="Link da imagem"
+              name="image"
+            />
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Cancelar</Button>
+          <Button onClick={onSubmitCreate}>Cadastrar</Button>
+        </DialogActions>
+      </>
+    </Dialog>
   )
 }
